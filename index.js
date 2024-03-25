b1e54290458c6e83c41de1c118496028d260b868
Angel Vargas <angel2311v@gmail.com>
2024-03-25 01:47:01 -0400

    complete

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+const mainElement = document.getElementById('main');
+mainElement.remove();
+const newHeader = document.createElement('h1');
+// Assign any desired content to the new header element, for example:
+newHeader.textContent = 'Hello, World!';
+newHeader.id = "victory";
+newHeader.textContent = "Angel is the champion";
